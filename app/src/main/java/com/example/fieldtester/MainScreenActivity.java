9753612f80c0f4c5a5cc97a7c92a83305569deb5
chill1203 <chill1203@naver.com>
2015-12-06 13:09:59 +0900

    initial commit of revised version of field_tester_t

---
+++ app/src/main/java/com/example/fieldtester/MainScreenActivity.java
@@ -0,0 +1,49 @@
+package com.example.fieldtester;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+public class MainScreenActivity extends Activity{
+	
+	ImageButton btnViewProducts;
+	ImageButton btnNewProduct;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_screen);
+		
+		// Buttons
+		btnViewProducts = (ImageButton) findViewById(R.id.btnViewProducts);
+		btnNewProduct = (ImageButton) findViewById(R.id.btnCreateProduct);
+		
+		// view products click event
+		btnViewProducts.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				// Launching All products Activity
+				Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+				startActivity(i);
+				
+			}
+		});
+		
+		// view products click event
+		btnNewProduct.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				// Launching create new product activity
+				Intent i = new Intent(getApplicationContext(), NewProductActivity.class);
+				startActivity(i);
+				
+			}
+		});
+	}
+}
